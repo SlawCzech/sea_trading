0388e88ceda9a4c02a5cbdae242031d5ddd59bd5
SlawCzech <slaw.czech@gmail.com>
2022-11-23 22:42:35 +0100

    class decorator

--- position.py
+++ position.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 class Position:
     def __init__(self, latitude, longitude):
         if not (-90 <= latitude <= 90):
@@ -38,7 +41,7 @@ class Position:
         prefix, dot, suffix = format_spec.partition('.')
 
         if dot:
-            component_decimal_places = f'.{suffix}f'
+            component_format_spec = f'.{suffix}f'
 
         latitude = format(abs(self.latitude), component_format_spec)
         longitude = format(abs(self.longitude), component_format_spec)
@@ -53,8 +56,67 @@ class MarsPosition(Position):
     pass
 
 
-waw = EarthPosition(52.1, 21)
+def auto_repr(cls):
+    members = vars(cls)
 
-print(waw)
-print(repr(waw))
-print(f'{waw:.5f}')
+    if '__repr__' in members:
+        raise TypeError(f'{cls.__name__} already defines __repr__')
+
+    if '__init__' not in members:
+        raise TypeError(f'{cls.__name__} does not override __init__')
+
+    signature = inspect.signature(cls.__init__)
+    parameter_names = list(signature.parameters)[1:]
+
+    if not all(
+        isinstance(members.get(name, None), property)
+        for name in parameter_names
+     ):
+        raise TypeError(
+            f'Cannot apply auto_repr to {cls.__name__}, because not all __init__ parameters have matching properties')
+
+    def synthesized_repr(self):
+        return "{typename}({args})".format(
+            typename=cls.__name__,
+            args=', '.join(
+                "{name}={value!r}".format(
+                    name=name,
+                    value=getattr(self, name)
+                ) for name in parameter_names
+            )
+        )
+
+    setattr(cls, "__repr__", synthesized_repr)
+
+    return cls
+
+
+@auto_repr
+class Location:
+    def __init__(self, name, position):
+        self._name = name
+        self._position = position
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def position(self):
+        return self._position
+
+
+hong_kong = Location("Hong Kong", EarthPosition(22.29, 114.16))
+stockholm = Location("Stockholm", EarthPosition(59.33, 18.06))
+cape_town = Location("Cape Town", EarthPosition(-33.93, 18.42))
+rotterdam = Location("Rotterdam", EarthPosition(51.96, 4.47))
+maracaibo = Location("Maracaibo", EarthPosition(10.65, -71.65))
+
+# waw = EarthPosition(52.1, 21)
+#
+# print(waw)
+# print(repr(waw))
+# print(f'{waw:.5f}')
+
+
+print(repr(maracaibo))
